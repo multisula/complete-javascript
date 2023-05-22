f26b3a2247ec2e599af3e60341e38545b1814927
multisula <multisula@gmail.com>
2023-05-22 10:53:46 +0900

    Taking Decisions: if / else Statements

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -121,3 +121,31 @@ console.log(`String
 multiple
 lines`)
  */
+
+/* // Taking Decisions: if / else Statements
+const age = 15
+const isOldEnough = age >= 18
+if (isOldEnough) {
+  console.log('Sarah can start driving license 🚗')
+} else {
+  const yearsLeft = 18 - age
+  console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`)
+}
+
+const birthYear = 1991
+if(birthYear <= 2000){
+  let century = 20
+} else {
+  let century = 21
+}
+console.log(century)
+ */
+
+/* Type Conversion and Coercion
+ */
+const inputYear = "1991";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number("Jonas"));
+console.log(typeof NaN); // => number
