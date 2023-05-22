1e6180655acab330a253ebce2e5d070f926400e7
multisula <multisula@gmail.com>
2023-05-22 22:59:13 +0900

    truthy and falsy

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -156,3 +156,25 @@ console.log('I am ' + 23 + ' years old')
 console.log('23' - '10' - 3) // => 10
 console.log('23' / '2')
  */
+
+/* Truthy and False Values
+console.log(Boolean(0))
+console.log(Boolean(undefined))
+console.log(Boolean('Jonas'))
+console.log(Boolean({})) // => true
+console.log(Boolean(''))
+
+const money = 0
+if(money){
+  console.log("Dont't spend it all ;")
+} else {
+  console.log('You sould get a job!')
+}
+
+let height
+if(height) {
+  console.log('YAY! Height is defined')
+} else {
+  console.log('Height is UNDEFINED')
+}
+*/
